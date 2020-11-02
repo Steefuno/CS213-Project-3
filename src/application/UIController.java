865819af768c59bf4bd5d664011e9a0e3899a1c0
Steve <pickleman20@hotmail.com>
2020-11-02 00:31:59 -0500

    Fixed toggle group error in controller

--- src/application/UIController.java
+++ src/application/UIController.java
@@ -55,6 +55,9 @@ public class UIController {
     private RadioButton moneyMarketRadioOc;
 
     @FXML
+    private ToggleGroup accountTypeOc;
+
+    @FXML
     private CheckBox directDepositBox;
 
     @FXML
